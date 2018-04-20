0f31b844fa25f757ddb7f260b353aa53abe25d35
Janos Poor <poorjanos@gmail.com>
2018-04-20 11:06:02 +0200

    Add new segmentation logic and plots

--- SQL/segmentation_life_autouw.sql
+++ SQL/segmentation_life_autouw.sql
@@ -1,27 +1,197 @@
-SELECT   a.f_ivk,
-           a.f_modkod,
-           a.f_erkezes,
-           a.f_lezaras,
-           a.f_kecs,
-           CASE
-              WHEN f_modkod IN ('13217', '13410', '13411', '13412')
-              THEN
-                 'Easy_nincseü'
-              WHEN EXISTS
-                      (SELECT   1
-                         FROM   afc.t_afc_wflog_lin2 b
-                        WHERE   a.f_ivk = b.f_ivk
-                                AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
-                                                              b.f_logid) LIKE
-                                      '%Senior validálásra%')
-                   OR (EXISTS
-                          (SELECT   1
-                             FROM   t_irat_tevlog t1
-                            WHERE   t1.f_ivk = a.f_ivk
-                                    AND f_alirattipusid IN (1921, 1926, 2027)))
-              THEN
-                 'Hard_senior'
-              WHEN f_modkod IN
+DROP TABLE t_life_2017;
+COMMIT;
+
+CREATE TABLE t_life_2017
+AS
+     SELECT   a.f_ivk,
+              a.f_modkod,
+              a.f_erkezes,
+              a.f_lezaras,
+              a.f_kecs,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   afc.t_afc_wflog_lin2 b
+                           WHERE   a.f_ivk = b.f_ivk
+                                   AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
+                                                                 b.f_logid) LIKE
+                                         '%Senior validálásra%')
+                      OR (EXISTS
+                             (SELECT   1
+                                FROM   t_irat_tevlog t1
+                               WHERE   t1.f_ivk = a.f_ivk
+                                       AND f_alirattipusid IN
+                                                (1921, 1926, 2027)))
+                 THEN
+                    1
+                 ELSE
+                    0
+              END
+                 AS Senior_kockelb,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   fproposal@dl_peep x
+                           WHERE   a.f_ivk = x.proposal_idntfr
+                                   AND x.medical_reception = 'N')
+                 THEN
+                    1
+                 ELSE
+                    0
+              END
+                 AS eunyil,
+              SUM (b.f_int_end - b.f_int_begin) * 1440 AS ido_perc
+       FROM   kontakt.t_ajanlat_attrib a, afc.t_afc_wflog_lin2 b
+      WHERE       f_erkezes BETWEEN DATE '2017-01-01' AND DATE '2017-12-31'
+              AND a.f_ivk = b.f_ivk
+              AND f_termcsop = 'ÉLET'
+              AND f_modkod NOT IN ('12701', '12702')
+              AND f_lezaras IS NOT NULL
+              AND f_kecs_pg = 'Feldolgozott'
+              AND f_kecs <> 'Törölve'
+   GROUP BY   a.f_ivk,
+              a.f_modkod,
+              a.f_erkezes,
+              a.f_lezaras,
+              a.f_kecs,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   afc.t_afc_wflog_lin2 b
+                           WHERE   a.f_ivk = b.f_ivk
+                                   AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
+                                                                 b.f_logid) LIKE
+                                         '%Senior validálásra%')
+                      OR (EXISTS
+                             (SELECT   1
+                                FROM   t_irat_tevlog t1
+                               WHERE   t1.f_ivk = a.f_ivk
+                                       AND f_alirattipusid IN
+                                                (1921, 1926, 2027)))
+                 THEN
+                    1
+                 ELSE
+                    0
+              END,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   fproposal@dl_peep x
+                           WHERE   a.f_ivk = x.proposal_idntfr
+                                   AND x.medical_reception = 'N')
+                 THEN
+                    1
+                 ELSE
+                    0
+              END
+   ORDER BY   a.f_ivk,
+              a.f_modkod,
+              a.f_erkezes,
+              a.f_lezaras,
+              a.f_kecs,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   afc.t_afc_wflog_lin2 b
+                           WHERE   a.f_ivk = b.f_ivk
+                                   AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
+                                                                 b.f_logid) LIKE
+                                         '%Senior validálásra%')
+                      OR (EXISTS
+                             (SELECT   1
+                                FROM   t_irat_tevlog t1
+                               WHERE   t1.f_ivk = a.f_ivk
+                                       AND f_alirattipusid IN
+                                                (1921, 1926, 2027)))
+                 THEN
+                    1
+                 ELSE
+                    0
+              END,
+              CASE
+                 WHEN EXISTS
+                         (SELECT   1
+                            FROM   fproposal@dl_peep x
+                           WHERE   a.f_ivk = x.proposal_idntfr
+                                   AND x.medical_reception = 'N')
+                 THEN
+                    1
+                 ELSE
+                    0
+              END;
+
+COMMIT;
+ALTER TABLE t_life_2017
+ADD
+(
+alap_bo number,
+kieg_es_cs char(2)
+);
+COMMIT;
+DROP INDEX ivk;
+COMMIT;
+
+CREATE INDEX ivk
+   ON t_life_2017 (f_ivk);
+
+COMMIT;
+
+UPDATE   t_life_2017 a
+   SET   alap_bo =
+            (SELECT   SUM (c.init_sum_insured)
+               FROM   fproposal@dl_peep b, fcontract_coverage@dl_peep c
+              WHERE       a.f_ivk = b.proposal_idntfr
+                      AND b.contract_oid = c.contract_oid
+                      AND c.cntry_flg = 'HU'
+                      AND (c.main_coverage_flag = 'Y'
+                           OR coverage_code = '13400C'));
+
+COMMIT;
+
+UPDATE   t_life_2017 a
+   SET   kieg_es_cs = 'I'
+ WHERE   NOT EXISTS
+            (SELECT   1
+               FROM   fproposal@dl_peep b, fcontract_coverage@dl_peep c
+              WHERE   a.f_ivk = b.proposal_idntfr
+                      AND b.contract_oid = c.contract_oid
+                      AND coverage_code IN
+                               ('13400C',
+                                '13400G',
+                                '13400Y',
+                                '13400E',
+                                '13400F',
+                                '13400X',
+                                '13400A',
+                                '13400I',
+                                '13400U',
+                                '13400Z',
+                                '13400Q',
+                                '13400L',
+                                '13400J',
+                                '13400R',
+                                '13400M',
+                                '13400T',
+                                '13400H',
+                                '1340EH',
+                                '1340DH'));
+COMMIT;
+
+
+ALTER TABLE t_life_2017
+add
+(szegmens varchar2(20),
+szegmens_old varchar2(20));
+COMMIT;
+
+UPDATE t_life_2017 a
+set szegmens_old =  'S1' where a.f_modkod IN ('13217', '13410', '13411', '13412');
+
+UPDATE t_life_2017 a
+set szegmens_old =  'S3' where a.senior_kockelb = 1;
+
+UPDATE t_life_2017 a
+set szegmens_old =  'S2' where  szegmens_old is null and f_modkod IN
                          ('13401',
                           '13402',
                           '13408',
@@ -30,49 +200,13 @@ SELECT   a.f_ivk,
                           '13404',
                           '13405',
                           '13406',
-                          '13407')
-                   AND EXISTS
-                         (SELECT   1
-                            FROM   fproposal@dl_peep x
-                           WHERE   a.f_ivk = x.proposal_idntfr
-                                   AND x.medical_reception = 'N')
-              THEN
-                 'Middle_negeü_nyug'
-           END
-              AS Szegmens,
-           SUM (b.f_int_end - b.f_int_begin) * 1440 AS ido_perc
-    FROM   kontakt.t_ajanlat_attrib a, afc.t_afc_wflog_lin2 b
-   WHERE       f_erkezes BETWEEN DATE '2017-01-01' AND DATE '2017-12-31'
-           AND a.f_ivk = b.f_ivk
-           AND f_termcsop = 'ÉLET'
-           AND f_modkod NOT IN ('12701', '12702')
-           AND f_lezaras IS NOT NULL
-           AND f_kecs_pg = 'Feldolgozott'
-           AND f_kecs <> 'Törölve'
-GROUP BY   a.f_ivk,
-           a.f_modkod,
-           a.f_erkezes,
-           a.f_lezaras,
-           a.f_kecs,
-           CASE
-              WHEN f_modkod IN ('13217', '13410', '13411', '13412')
-              THEN
-                 'Easy_nincseü'
-              WHEN EXISTS
-                      (SELECT   1
-                         FROM   afc.t_afc_wflog_lin2 b
-                        WHERE   a.f_ivk = b.f_ivk
-                                AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
-                                                              b.f_logid) LIKE
-                                      '%Senior validálásra%')
-                   OR (EXISTS
-                          (SELECT   1
-                             FROM   t_irat_tevlog t1
-                            WHERE   t1.f_ivk = a.f_ivk
-                                    AND f_alirattipusid IN (1921, 1926, 2027)))
-              THEN
-                 'Hard_senior'
-              WHEN f_modkod IN
+                          '13407') and eunyil = 1;
+COMMIT;
+
+
+UPDATE t_life_2017 a
+set szegmens =  'S1' where a.f_modkod IN ('13217', '13410', '13411', '13412')
+                    or (f_modkod IN
                          ('13401',
                           '13402',
                           '13408',
@@ -81,39 +215,13 @@ GROUP BY   a.f_ivk,
                           '13404',
                           '13405',
                           '13406',
-                          '13407')
-                   AND EXISTS
-                         (SELECT   1
-                            FROM   fproposal@dl_peep x
-                           WHERE   a.f_ivk = x.proposal_idntfr
-                                   AND x.medical_reception = 'N')
-              THEN
-                 'Middle_negeü_nyug'
-           END
-ORDER BY   a.f_ivk,
-           a.f_modkod,
-           a.f_erkezes,
-           a.f_lezaras,
-           a.f_kecs,
-           CASE
-              WHEN f_modkod IN ('13217', '13410', '13411', '13412')
-              THEN
-                 'Easy_nincseü'
-              WHEN EXISTS
-                      (SELECT   1
-                         FROM   afc.t_afc_wflog_lin2 b
-                        WHERE   a.f_ivk = b.f_ivk
-                                AND afc.afc_wflog_intezkedes (b.f_ivkwfid,
-                                                              b.f_logid) LIKE
-                                      '%Senior validálásra%')
-                   OR (EXISTS
-                          (SELECT   1
-                             FROM   t_irat_tevlog t1
-                            WHERE   t1.f_ivk = a.f_ivk
-                                    AND f_alirattipusid IN (1921, 1926, 2027)))
-              THEN
-                 'Hard_senior'
-              WHEN f_modkod IN
+                          '13407') and alap_bo <= 500000 and kieg_es_cs = 'I');
+
+UPDATE t_life_2017 a
+set szegmens =  'S3' where a.senior_kockelb = 1;
+
+UPDATE t_life_2017 a
+set szegmens =  'S2' where  szegmens is null and f_modkod IN
                          ('13401',
                           '13402',
                           '13408',
@@ -122,14 +230,19 @@ ORDER BY   a.f_ivk,
                           '13404',
                           '13405',
                           '13406',
-                          '13407')
-                   AND EXISTS
-                         (SELECT   1
-                            FROM   fproposal@dl_peep x
-                           WHERE   a.f_ivk = x.proposal_idntfr
-                                   AND x.medical_reception = 'N')
-              THEN
-                 'Middle_negeü_nyug'
-           END
-           
-           
+                          '13407') and eunyil = 1;
+COMMIT;
+
+
+ALTER TABLE t_life_2017
+ADD
+(
+erk_szerz number);
+COMMIT;
+
+
+UPDATE   t_life_2017 a
+   SET   erk_szerz =
+            f_lezaras - f_erkezes - bnap_db@dl_peep (f_lezaras, f_erkezes);
+
+COMMIT;
--- main.R
+++ main.R
@@ -10,6 +10,7 @@ library(lubridate)
 library(stringr)
 library(tidyr)
 library(scales)
+library(xlsx)
 
 #Connect to Oracle (Kont@kt:MMBDBIKON)
 # Set JAVA_HOME, set max. memory, and load rJava library
@@ -43,18 +44,30 @@ jdbcConnection <-
     password = kontakt$pwd
   )
 
-# Get SQL script
-readQuery <-
-  function(file)
-    paste(readLines(file, warn = FALSE), collapse = "\n")
-segement_data <-
-  readQuery(here::here("SQL", "segmentation_life_autouw.sql"))
 
 # Run query
-t_life_2017 <- dbGetQuery(jdbcConnection, segement_data)
+t_life_2017 <- dbGetQuery(jdbcConnection, "select * from t_life_2017")
 
 # Close connection
 dbDisconnect(jdbcConnection)
+
+
+
+
+
+#########################################################################################
+# Transform Data ########################################################################
+#########################################################################################
+
+# Clean dataset -------------------------------------------------------------------------
+t_life_2017 <- t_life_2017[!is.na(t_life_2017$SZEGMENS), ]
+
+# Add prod name
+prod_names <- read.xlsx(here::here("Data", "t_life_prods.xlsx"), sheetIndex = 1, 
+                        stringsAsFactors = FALSE, encoding = 'UTF-8'
+  )
+
+t_life_2017 <- t_life_2017 %>% left_join(prod_names, by = c("F_MODKOD"))
 
 
 # Save res to local storage--------------------------------------------------------------
@@ -62,3 +75,45 @@ write.csv(t_life_2017, here::here("Data", "t_life_2017.csv"), row.names = F)
 
 
 
+#########################################################################################
+# Aggregate #############################################################################
+#########################################################################################
+
+segments <- t_life_2017 %>% group_by(SZEGMENS) %>% 
+                            summarize(DARAB = n(),
+                                      FTE = sum(IDO_PERC, na.rm = TRUE)/60/7/220,
+                                      LETSZAM = sum(IDO_PERC, na.rm = TRUE)/60/7/220*1.3)
+
+write.xlsx(segments, here::here("Data", "segs_to_excel.xlsx"))
+
+
+lead_time <- t_life_2017 %>% group_by(SZEGMENS) %>% 
+                          summarize(DARAB = n(),
+                                    ERK_SZERZ = mean(ERK_SZERZ, na.rm = TRUE))
+
+write.xlsx(lead_time, here::here("Data", "leadtime_to_excel.xlsx"))
+
+
+# Plot -----------------------------------------------------------------------------------
+prod_struct <- t_life_2017 %>%
+  filter(RNEV != "Egyéb") %>%
+  group_by(SZEGMENS, RNEV) %>%
+  summarise(TOTAL = n()) %>%
+  mutate(RATIO = TOTAL / sum(TOTAL))
+
+ggplot(prod_struct, aes(factor(1), RATIO, fill = factor(RNEV), label = RNEV)) +
+  geom_bar(stat = "identity") +
+  geom_text(check_overlap = TRUE, size = 2, position = position_stack(vjust = 0.5)) +
+  facet_grid(. ~ SZEGMENS) +
+  facet_grid(. ~ SZEGMENS) +
+  scale_y_continuous(label = percent) +
+  scale_x_discrete(breaks = NULL) +
+  theme(legend.position = "none") +
+  labs(
+    x = "SZEGMENS",
+    y = "ARÁNY"
+  )
+ggsave(here::here("Reports", "prod_per_seg.png"),
+  width = 10,
+  dpi = 500
+)
